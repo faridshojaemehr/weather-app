c111ff17e42b301e7f6ad8a99d0d2fad3f6bfbac
f.shojaeimehr <f.shojaeimehr>
2023-07-23 01:42:07 +0330

    fixed input & style

--- src/app/components/main/pages/home/home.module.ts
+++ src/app/components/main/pages/home/home.module.ts
@@ -5,9 +5,10 @@ import { WeatherComponent } from '../weather/weather/weather.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
 import { WeatherModule } from '../weather/weather.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, WeatherModule],
+  imports: [CommonModule, HomeRoutingModule, WeatherModule, SharedModule],
 })
 export class HomeModule {}
